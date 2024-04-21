50d983e6241470847a6e2bc6de368409551703b3
HanisSyafiqah23 <147361980+HanisSyafiqah23@users.noreply.github.com>
2024-04-21 11:52:42 +0800

    Add files via upload

---
+++ ADMIN/UserModel2.kt
@@ -0,0 +1,8 @@
+package com.example.waveoffoodadminOrder.model
+
+class UserModel2(
+    val name: String? = null,
+    val email: String? = null,
+    val password: String? = null,
+    s: String
+)
